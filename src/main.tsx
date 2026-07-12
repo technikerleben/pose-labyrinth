9852b47277b27a4ceabcc50b6f4858a5815d03c3
technikerleben <christophvillis@googlemail.com>
2026-07-12 13:17:29 +0200

    Load hand number styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import Launcher from './Launcher';
 import './styles.css';
+import './hand-numbers.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
