1b41cec623c79c26d7790a00cb981362eeace038
technikerleben <christophvillis@googlemail.com>
2026-07-12 21:57:16 +0200

    Load dance game styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import Launcher from './Launcher';
 import './styles.css';
 import './hand-numbers.css';
+import './dance-game.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
